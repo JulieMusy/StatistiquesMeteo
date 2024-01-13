be3545593911938814036475dd040143196f11cc
Julie Musy <76889730+JulieMusy@users.noreply.github.com>
2024-01-13 10:57:36 +0100

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -25,7 +25,7 @@ DICT_ID_VERS_NOM = dict( zip( ID_STATIONS_DF["ID"] , ID_STATIONS_DF["Nom"] ) )
 def getDataOneMonth( YYYYMM ) :
   # I)2) Téléchargement
   df = pd.read_csv('https://donneespubliques.meteofrance.fr/donnees_libres/Txt/Synop/Archive/synop.'+str(YYYYMM)+".csv.gz",sep=';') 
-
+  print(YYYYMM)
   # II)2) Application du dictionnaire sur le nom des colonnes
   df = df.rename( columns = DICT_COLUMN_NAMES )    
 
